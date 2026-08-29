75659bdd5097518b00363c212e78a03976a912f6
devenric <engato@alumnatflorida.es>
2026-08-29 11:36:28 +0200

    add frontend

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Hanken-Grotesk",
+    cssVariable: "--font-hanken-grotesk",
+  }]
+});
